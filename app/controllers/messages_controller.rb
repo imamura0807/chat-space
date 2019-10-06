3b29c05bf340050e4c90109b776aaf4bd8b62ac0
imamura0807 <timamura8787@gmail.com>
2019-10-06 16:30:25 +0900

    修正しました_r2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,13 +11,13 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-    respond_to do |format|
-    format.html {redirect_to group_messages_path(params[:group_id])}
-    format.json
-    end
+      respond_to do |format|
+        format.html {redirect_to group_messages_path(params[:group_id])}
+        format.json
+      end
     else
-    flash[:notice] = 'メッセージを入力してください'
-    render :index
+      flash[:notice] = 'メッセージを入力してください'
+      render :index
     end
   end
 
